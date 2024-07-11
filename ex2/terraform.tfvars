2d7739453ebdef4034ee248d03848d967981b10a
pervez.salauddin <pervez.salauddin@gmail.com>
2024-07-12 02:42:02 +0300

    ex2 files

---
+++ ex2/terraform.tfvars
@@ -0,0 +1,4 @@
+ami_value           = "ami-0b72821e2f351e396"
+instance_type_value = "t2.micro"
+subnet_id_value     = "subnet-0e23c358ee365a1d1"
+vpc_security_group_ids_value = ["sg-096464a9ed8c9a20b"]
---
+++ ex2/vars.tf
@@ -0,0 +1,15 @@
+variable "ami_value" {
+  description = "value for the ami"
+}
+
+variable "instance_type_value" {
+  description = "value for instance type"
+}
+
+variable "subnet_id_value" {
+  description = "value for the subnet id"
+}
+
+variable "vpc_security_group_ids_value" {
+    description = "value for security groups"
+}
